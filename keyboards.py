578ebce6e62058e862c672d4a72e527b9739a3ee
drars38 <144019477+drars38@users.noreply.github.com>
2024-07-03 22:40:23 +0800

    Add files via upload
    переделал чат поддержку

--- keyboards.py
+++ keyboards.py
@@ -1,40 +1,31 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-# Основная клавиатура
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Написать коллеге')],
-    [KeyboardButton(text='О компании'), KeyboardButton(text='Задать вопрос')],
-    [KeyboardButton(text='Инвесторам и акционерам'), KeyboardButton(text='Клиентам')],
-    [KeyboardButton(text='Войти в аккаунт', request_contact=True)]
-], resize_keyboard=True, input_field_placeholder='Выберите пункт меню.')
-
-
-# Клавиатура для внутреннего меню
-inner_main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Тех. поддержка'), KeyboardButton(text='Развлечения')],
-    [KeyboardButton(text='Написать коллеге')],
-    [KeyboardButton(text='Назад')]
-], resize_keyboard=True, input_field_placeholder='Выберите пункт меню.')
-
-# Клавиатура подтверждения отправки сообщения
-message_confirm = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="Отправить сообщение")],
-    [KeyboardButton(text='Назад')]
-], input_field_placeholder='Выберите пункт меню.')
-
-# Inline клавиатура для настроек
-settings = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Ознакомиться', url='https://example.com')],
-])
-
-login = ReplyKeyboardMarkup(
-
+# Основная клавиатура для обычных пользователей
+user_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text='Отправить контакт', request_contact=True)]
-    ]
+        [KeyboardButton(text='Помощь')],
+        [KeyboardButton(text='Зарегистрироваться', request_contact=True)]
+    ],
+    resize_keyboard=True
+)
 
+# Основная клавиатура для администраторов
+admin_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Помощь')],
+        [KeyboardButton(text='Зарегистрироваться', request_contact=True)],
+        [KeyboardButton(text='Список пользователей')],
+        [KeyboardButton(text='Неотвеченные сообщения')]
+    ],
+    resize_keyboard=True
 )
 
 
-
+# Inline-клавиатура для админов
+def create_admin_inline_keyboard(message_id):
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='Ответить', callback_data=f'reply_{message_id}')]
+        ]
+    )
+    return keyboard
